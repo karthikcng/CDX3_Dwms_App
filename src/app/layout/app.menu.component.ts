eb700681c834fa3c775a02e824b17e2fc2ea8346
ankushadharaj <ankushadharaj@gmail.com>
2025-01-13 07:14:56 +0000

    path changes and correct docker/ nginx config

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -23,7 +23,7 @@ export class AppMenuComponent implements OnInit {
                     {
                         label: 'DWMS Dashboard',
                         icon: 'pi pi-fw pi-chart-pie',
-                        routerLink: ['/dwms']
+                        routerLink: ['/menu']
                     }
                 ]
             },
